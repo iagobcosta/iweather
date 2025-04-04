13b5b42094d69f24626bf886a0d6031a0b379c32
Iago Barbosa <iago.barbosa@somapay.com.br>
2025-04-04 12:16:55 -0300

    Adicionando novos teste e icone

---
+++ src/screens/Search/search.test.tsx
@@ -0,0 +1,24 @@
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+} from "@__tests__/utils/customRender"
+import { mockCityApiResponse } from "@__tests__/mocks/api/mockCityApiResponse"
+import { Search } from "@screens/Search"
+import { api } from "@services/api"
+
+describe("Screen: Search", () => {
+  it("should be show city option", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityApiResponse })
+
+    render(<Search />)
+
+    const searchInput = screen.getByTestId("search-input")
+    fireEvent.changeText(searchInput, "São Paulo")
+
+    const option = await waitFor(() => screen.findByText(/paulo/i))
+
+    expect(option).toBeTruthy()
+  })
+})
